ce11b934719c439117fc4835fd6e0b0fb8afb74d
priyankachaudhary77 <priyankaart80@gmail.com>
2026-09-21 10:58:25 -0500

    Add files via upload

---
+++ JavaScriptFirstProgra/script.js
@@ -0,0 +1,6 @@
+console.log("Hello, JavaScript!");
+console.log("My first program is running.");
+console.log("we are in CIS 483!");
+console.log("hello world");
+   console.log("Our favorite number is 7");
+   console.log("We are learning JavaScript in CIS 483.");
